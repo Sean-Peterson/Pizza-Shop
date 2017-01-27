a3c384a214177c9b096b44709f6dcedb5797e884
Sean Peterson <seanpeterson11@gmail.com>
2017-01-27 10:05:35 -0800

    add front end and link to backend. Ordering alerts a price

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,36 @@ function PizzaPie(size, toppings) {
   this.toppings = toppings;
 };
 
-var newPizza = function (n) {
-  new PizzaPie(n)
-};
-
 PizzaPie.prototype.price = function() {
-  return this.size * 3 + this.toppings;
+  return this.size * 3 + this.toppings.length;
 };
 
 //FRONT-END
 $(document).ready(function() {
+  $('form').submit(function(event) {
+    event.preventDefault();
+
+    var toppings = []
+    $("input:checkbox[name=topping]:checked").each(function(){
+      var pizzaTopping = $(this).val();
+      toppings.push(pizzaTopping);
+    });
+
+    var size = $('input[name=size]:checked').val();
+
+    var customPizza = new PizzaPie(size, toppings);
+
+    alert(customPizza.price())
+
+
+
+
+
+    // var customerPizza = newPizza()
+
+
+  });
+
+
 
 });
