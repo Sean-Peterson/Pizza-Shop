22ea27b7a1e7d437f6d3798879dc58f75ac433f0
Sean Peterson <seanpeterson11@gmail.com>
2017-01-27 12:42:49 -0800

    add functionality to hide form and display price of pizza after clicking submit

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,15 @@ PizzaPie.prototype.price = function() {
   return this.size * 3 + this.toppings.length;
 };
 
+var add = function(n1,n2) {
+  return  n1 + n2;
+}
+
 //FRONT-END
 $(document).ready(function() {
-  $('form').submit(function(event) {
+  $('#orderForm').submit(function(event) {
     event.preventDefault();
-
+    //Get toppings from form checkboxes
     var toppings = []
     $("input:checkbox[name=topping]:checked").each(function(){
       var pizzaTopping = $(this).val();
@@ -20,18 +24,32 @@ $(document).ready(function() {
     });
 
     var size = $('input[name=size]:checked').val();
-
     var customPizza = new PizzaPie(size, toppings);
+    var priceOne = customPizza.price()
 
-    alert(customPizza.price())
+    //Start ordering form two
+    //Get toppings from form checkboxes
+    var toppingsTwo = []
+    $("input:checkbox[name=topping-two]:checked").each(function(){
+      var pizzaToppingTwo = $(this).val();
+      toppingsTwo.push(pizzaToppingTwo);
+    });
 
+    var size = $('input[name=size-two]:checked').val();
+    var customPizzaTwo = new PizzaPie(size, toppings);
+    var priceTwo = customPizzaTwo.price();
 
+    if (customPizzaTwo.price() > 0){
+      $('#total-price').text(' ' + '$' + add(priceOne, priceTwo))
+    } else {
+      $('#total-price').text(' ' + '$' + priceOne)
+    }
 
-
-
-    // var customerPizza = newPizza()
-
-
+    $('#orderForm').hide();
+    $('.showPrice').show();
+  });
+  $('#second-pie-form').click(function(){
+    $('#secondPie').toggle();
   });
 
 
